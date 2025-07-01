40d54d79d1a1415be6324191595cc123eed5bc47
Klinsic87 <klinsic@gmail.com>
2025-07-01 19:52:31 +0930

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,11 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDbPmWgq7N3ZBn15dU6KMfsxrEmfKA0-G8",
   authDomain: "job-scheduler-app.firebaseapp.com",
@@ -14,7 +16,6 @@ const firebaseConfig = {
   measurementId: "G-19Z18D3PV0"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
